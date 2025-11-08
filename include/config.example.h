3164133b96f5a03a1e78b097a09bdf8f4e5a00a3
Piotr Górski <piotr@prokocim.org>
2025-11-08 18:58:56 +0100

    asd

---
+++ include/config.example.h
@@ -0,0 +1,18 @@
+// config.example.h
+// ----------------------------------------------------------
+// Kopiuj ten plik do config.h i uzupełnij własnymi danymi.
+// Plik config.h nie jest śledzony przez git (patrz .gitignore).
+// ----------------------------------------------------------
+
+#pragma once
+
+// WiFi credentials
+#define WIFI_SSID      "SSID"
+#define WIFI_PASSWORD  "Password"
+
+// API endpoint
+#define API_URL        "http://ontime.example.com/api/poll"
+
+// Czas odświeżania danych [ms]
+#define REFRESH_INTERVAL_MS 1000 // 1 sekunda
+
